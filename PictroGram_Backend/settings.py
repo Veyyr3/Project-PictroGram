5e8884f5be303bf6bde5825e00a4bd269f53b1e5
veyyr3 <dimapolska967@gmail.com>
2025-06-29 20:22:18 +0600

    add settings

--- PictroGram_Backend/settings.py
+++ PictroGram_Backend/settings.py
@@ -102,7 +102,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru-ru'
 
 TIME_ZONE = 'UTC'
 
@@ -115,6 +115,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = (BASE_DIR / 'static',)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
--- PictroGram_Backend/urls.py
+++ PictroGram_Backend/urls.py
@@ -1,22 +1,20 @@
-"""
-URL configuration for PictroGram_Backend project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+# main urls.py
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
+# импорт контроллеров
+# from products.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path('', index, name='index'),
+    # path('products/', include('products.urls', namespace='products')),
+    # path('users/', include('users.urls', namespace='users'))
 ]
+
+# если в режиме разработки
+if settings.DEBUG:
+    # добавить место хранения фотографий записей из БД
+    urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT) 
