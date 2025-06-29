3a27b67f7809781ae88d9b0239f4d46f167f1e55
veyyr3 <dimapolska967@gmail.com>
2025-06-29 22:12:50 +0600

    configured settings, add IMAGE base HTML

--- PictroGram_Backend/urls.py
+++ PictroGram_Backend/urls.py
@@ -5,11 +5,11 @@ from django.conf.urls.static import static
 from django.conf import settings
 
 # импорт контроллеров
-# from products.views import index
+from APPS.image.views import index
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    # path('', index, name='index'),
+    path('', index, name='index'),
     # path('products/', include('products.urls', namespace='products')),
     # path('users/', include('users.urls', namespace='users'))
 ]
