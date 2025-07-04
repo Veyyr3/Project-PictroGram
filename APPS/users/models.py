04728195d930dbae30e4bfc1f83824003da946a9
veyyr3 <dimapolska967@gmail.com>
2025-07-04 19:53:34 +0600

    BE: update Users model

--- APPS/users/models.py
+++ APPS/users/models.py
@@ -1,3 +1,22 @@
+# users/models.py
 from django.db import models
+from django.contrib.auth.models import AbstractUser # импортировать модель пользователей
 
-# Create your models here.
+# Наследуется от AbstractUser
+class User(AbstractUser):
+    # описание
+    bio = models.TextField(blank=True, null=True)
+    # аватар
+    avatar = models.ImageField(upload_to='users_avatars', null=True, blank=True)
+
+class Subscriptions(models.Model):
+    # подписчик
+    subscriber = models.ForeignKey("User", verbose_name="Подписчик", on_delete=models.CASCADE, related_name='subscriptions_made')
+    # на кого подписались
+    subscribed_to = models.ForeignKey("User", verbose_name="На кого подписались", on_delete=models.CASCADE, related_name='subscribers')
+    # дата подписки
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        # чтобы пользователи не могли подписываться друг на друга несколько раз
+        unique_together = ('subscriber', 'subscribed_to')
