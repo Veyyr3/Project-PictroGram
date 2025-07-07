436dea98b2cd4ad52947e31467b92cc3c86cf496
veyyr3 <dimapolska967@gmail.com>
2025-07-07 15:43:45 +0600

    BE: register Subscriptions to Admin

--- APPS/users/admin.py
+++ APPS/users/admin.py
@@ -5,4 +5,7 @@ from users.models import User, Subscriptions
 
 # Register your models here.
 admin.site.register(User)
-admin.site.register(Subscriptions)
+
+@admin.register(Subscriptions)
+class SubscriptionsAdmin(admin.ModelAdmin):
+    list_display = ['subscriber', 'subscribed_to', 'created_at']
