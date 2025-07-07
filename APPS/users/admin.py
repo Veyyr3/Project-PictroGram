72a65d13a166fdfdee33b214b69b63a4dafa0f92
veyyr3 <dimapolska967@gmail.com>
2025-07-07 15:39:32 +0600

    BE: add to Admin

--- APPS/users/admin.py
+++ APPS/users/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
 # модели
-from users.models import User
+from users.models import User, Subscriptions
 
 # Register your models here.
 admin.site.register(User)
+admin.site.register(Subscriptions)
