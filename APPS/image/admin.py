eb0f693095826c5b5e7f7860cf22780f94953ad5
veyyr3 <dimapolska967@gmail.com>
2025-07-09 13:35:52 +0600

    BE: register Comments to admin

--- APPS/image/admin.py
+++ APPS/image/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 
-from image.models import Images, Likes
+from image.models import Images, Likes, Comments
 
 # Register your models here.
 admin.site.register(Images)
 admin.site.register(Likes)
+admin.site.register(Comments)
